b4d7f6f79c4d637874b8bd4b5c4827e023bff2ba
Sriram S <sriram@innervoice.in>
2015-07-13 15:01:40 +0530

    Refactored code for Network

--- ch10_ml2_mech_driver.py
+++ ch10_ml2_mech_driver.py
@@ -1,11 +1,8 @@
-import sys
-import re
-import socket
-
-from neutron.db import api as db
-from neutron.plugins.ml2 import db as ml2_db
+try:
+    from neutron.openstack.common import log as logger
+except ImportError:
+    from oslo_log import log as logger 
 from neutron.plugins.ml2 import driver_api as api
-from neutron.openstack.common import log as logger
 
 driver_logger = logger.getLogger(__name__)
 
@@ -15,21 +12,69 @@ class CookbookMechanismDriver(api.MechanismDriver):
     def initialize(self):
         driver_logger.error("Inside Mech Driver Initialize")
 
+    def _log_network_information(method_name, network_context):
+        driver_logger.error("*********** %s *********" % (method_name))
+	driver_logger.error("Network Type: ".network_context['provider:network_type'])
+        driver_logger.error("********************")
 
     def create_network_precommit(self, context):
-        driver_logger.error("Inside Nework Precommit")
         network_context = context.current
-        plugin_context = context._plugin_context
-	driver_logger.error(network_context)
-        driver_logger.error("******")
-	driver_logger.error(plugin_context)
+	_log_network_information("Create Network PreCommit", network_context)
 
 
     def create_network_postcommit(self, context):
-        driver_logger.error("Inside Nework Postcommit")
         network_context = context.current
-        plugin_context = context._plugin_context
-	driver_logger.error(network_context)
-        driver_logger.error("******")
-	driver_logger.error(plugin_context)
+	_log_network_information("Create Network PostCommit", network_context)
 	
+    def update_network_precommit(self, context):
+        pass
+
+    def update_network_postcommit(self, context):
+        pass
+
+    def delete_network_precommit(self, context):
+        pass
+
+    def delete_network_postcommit(self, context):
+        pass
+
+    def create_subnet_precommit(self, context):
+        pass
+
+    def create_subnet_postcommit(self, context):
+        pass
+
+    def update_subnet_precommit(self, context):
+        pass
+
+    def update_subnet_postcommit(self, context):
+        pass
+
+    def delete_subnet_precommit(self, context):
+        pass
+
+    def delete_subnet_postcommit(self, context):
+        pass
+
+    def create_port_precommit(self, context):
+        pass
+
+    def create_port_postcommit(self, context):
+        pass
+
+    def update_port_precommit(self, context):
+        pass
+
+    def update_port_postcommit(self, context):
+        pass
+
+    def delete_port_precommit(self, context):
+        pass
+
+    def delete_port_postcommit(self, context):
+        pass
+
+    def bind_port(self, context):
+        pass
+
+
